499125881aecf5902a833b1bcff3e272bf805041
loaki07 <loakesh.i@gmail.com>
2022-02-09 11:01:25 +0530

    add: single & milti line comments

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,10 +2,12 @@
 
 int main() 
 { 
+    // print welcome messages to the terminal
     std::cout << "you are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
     std::cout << "you need to enter the correct codes to continue...";
 
+    // declare 3 number code
     int a =  4;
     const int b = 3;
     const int c = 2;
@@ -15,6 +17,12 @@ int main()
     int sum = a + b + c;
     int product = a * b * c;
 
+    /*
+    this is a 
+    multi line comment
+    */
+
+    // print sum and product to the terminal
     std::cout << std::endl;
     std::cout << sum;
     std::cout << std::endl;
